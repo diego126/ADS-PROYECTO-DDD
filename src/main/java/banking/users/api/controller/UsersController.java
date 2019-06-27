ce1db5b87f791067a1a8ace0345c50eb363b8999
Diego Tarzona <diego126.tn@gmail.com>
2019-06-26 19:47:47 -0500

    Carga demo  en clases

--- src/main/java/banking/users/api/controller/UsersController.java
+++ src/main/java/banking/users/api/controller/UsersController.java
@@ -46,7 +46,7 @@ public class UsersController {
 			if (userAuthDto.isAuthenticated()) {
 				return new ResponseEntity<Object>(userAuthDto, HttpStatus.OK);
 			}
-			return this.responseHandler.getResponse("Usuario y/o contraseña incorrecta. Por favor verifiquwe.", HttpStatus.NOT_FOUND);
+			return this.responseHandler.getResponse("Mensaje de ejemplo. Por favor verifiquwe.", HttpStatus.NOT_FOUND);
 		} catch(IllegalArgumentException ex) {
 			return this.responseHandler.getAppCustomErrorResponse(ex.getMessage());
 		} catch(Exception ex) {
